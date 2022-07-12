75d49efc475f380ca94f1a8b23d1e4d7b3ba87c9
vincent <vincnttan@gmail.com>
2022-07-12 20:15:06 +0100

    Improve config.py

--- config.py
+++ config.py
@@ -1,7 +1,14 @@
 import yaml
+import os
 import addict
 
+# You can store your config.yaml location here if you are running this with python and don't want to reinput it everytime
+yaml_location = None
 
-yaml_location = input('What is the full path to your config.yaml file? (ie. /Users/youruser/BookBuilder/config.yaml): ')
+if yaml_location is None:
+    yaml_location = input('What is the full path to your config.yaml file? (ie. /Users/youruser/BookBuilder/config.yaml): ')
+while not os.path.exists(yaml_location):
+        yaml_location = input('The location you have input does not exist, please try again: ')
+
 with open(yaml_location, "r") as f:
     config = addict.Dict(yaml.safe_load(f))
