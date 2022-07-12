e2039cde661aa43c711a7919add895edbee05ff6
raccrompton <raccrompton@gmail.com>
2022-07-12 14:04:24 +0200

    Update config.py

--- config.py
+++ config.py
@@ -12,14 +12,14 @@ SPEEDS = ['blitz,rapid,classical,correspondence']
 #comma separated Formats to include in the analysis
 RATINGS = ['1600,1800,2000,2200,2500'] 
 #Ratings of the players to include in the analysis
-MOVES = 10
-#The number of most played moves to search over for the best move (minimum 5)
+MOVES = 5
+#The number of most played moves to search over for the best move, and the number of opponent moves to consider as continuations (minimum 5, maximum 12)
 
 #MOVE SELECTION SETTINGS
 DEPTHLIKELIHOOD = 0.05
-#once cumulative line likelihood reaches this threshold, no futher continuations will be added (in percentage so 0.0025 = 0.25%)
+#once cumulative line likelihood reaches this threshold, no futher continuation moves will be added (in percentage so 0.0025 = 0.25% of occuring in your games)
 ALPHA = 0.001
-# The confidence interval alpha (EG 0.05 = 95% CI), for deciding the lower bounds of how good a move's winrate is.
+#The confidence interval alpha (EG 0.05 = 95% CI), for deciding the lower bounds of how good a move's winrate is. If this is high, moves with less data will be picked more often
 MINPLAYRATE = 0.001
 #minimum frequency for a move to be played in a position to be considered as a 'best move' candidate (0.05= 5%)
 MINGAMES = 19 
@@ -29,21 +29,21 @@ CONTINUATIONGAMES = 10
 
 #ENGINE SETTINGS
 ENGINEPATH = r"/Users/AM/Downloads/BookBuilder/Stockfish"
-#the filepath where the engine is stored on your computer, so it can be accessed. Keep the 'r' character
+#the filepath where the engine is stored on your computer, so it can be accessed. Keep the 'r' character and the quotes around the file path
 CAREABOUTENGINE = 1
 #care about engine eval of position or engine finishing = 1, dont care = 0
-ENGINEDEPTH = 5
-#how deep engine should evaluate best moves
+ENGINEDEPTH = 10
+#how deep engine should evaluate best moves. For many openings a high depth is recommended, but takes longer.
 ENGINEFINISH = 1
-#if we want the engine to complete lines to cumulative likelihood where data is insufficient, 1. Otherwise 0, and lines will end where there's no good human data
+#if we want the engine to complete lines to cumulative likelihood where data is insufficient, 1. Otherwise 0, and lines will end where there's no good human data to pick from
 SOUNDNESSLIMIT = -99
-#maximum centipawns we are willing to be down in engine eval, provided the winrate is better (-300 = losing by 3 pawns in eval). We never give up a forced mate, however.
+#maximum centipawns we are willing to be down in engine eval, provided the winrate is better (-300 = losing by 3 pawns in eval). We never give up a forced mate, however
 MOVELOSSLIMIT = -99
 #maximum centipawns we are willing to lose vs engine analysis pre move to play a higher winrate move
 IGNORELOSSLIMIT = 300
 #centipawns advantage above which we won't care if we play a move that hits our loss limit, if it has a higher win rate (is easier to win)
-ENGINETHREADS = 10 #max 20
+ENGINETHREADS = 1 #max for me 20
 #how many threads you want the engine to use (check your comp and set 1 if unsure)
-ENGINEHASH = 8192 #4096 #max 10240
+ENGINEHASH = 32 #max for me 10240
 #how much hash you want the engine to use (check your comp and set to 16 if unsure)
 
