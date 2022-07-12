6c2ba26d5e2c89b771912bc2dd2a1265aa68d893
vincent <vincnttan@gmail.com>
2022-07-12 20:01:34 +0100

    Make config ask for config file location

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ import yaml
 import addict
 
 
-# yaml_location = input('What is the full path to your config.yaml file? ')
-yaml_location = '/Users/vincenttan/Code/work/BookBuilder/config.yaml'
+yaml_location = input('What is the full path to your config.yaml file? ie. /Users/youruser/BookBuilder/config.yaml')
 with open(yaml_location, "r") as f:
     config = addict.Dict(yaml.safe_load(f))
